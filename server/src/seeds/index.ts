ef37534342d72b56b0b220a02af9fa90895487ee
JohnBrunson <JohnFBrunson@gmail.com>
2025-02-05 23:07:50 -0800

    User model, auth routes, middleware added

--- server/src/seeds/index.ts
+++ server/src/seeds/index.ts
@@ -1,5 +1,3 @@
-import { seedVolunteers } from './volunteer-seeds.js';
-import { seedWork } from './work-seeds.js';
 import sequelize from '../config/connection.js';
 
 const seedAll = async (): Promise<void> => {
@@ -7,11 +5,11 @@ const seedAll = async (): Promise<void> => {
     await sequelize.sync({ force: true });
     console.log('\n----- DATABASE SYNCED -----\n');
     
-    await seedVolunteers();
-    console.log('\n----- VOLUNTEERS SEEDED -----\n');
+    // await seedVolunteers();
+    // console.log('\n----- VOLUNTEERS SEEDED -----\n');
     
-    await seedWork();
-    console.log('\n----- WORK SEEDED -----\n');
+    // await seedWork();
+    // console.log('\n----- WORK SEEDED -----\n');
     
     process.exit(0);
   } catch (error) {
