4f813638812e61e48de651aeb825ef7a9c96a7c2
JohnBrunson <JohnFBrunson@gmail.com>
2025-02-07 22:04:56 -0800

    Revised random meal route. Tested and working

--- server/src/routes/api/mealroutes.ts
+++ server/src/routes/api/mealroutes.ts
@@ -1,17 +1,32 @@
+//Future Development: Just convert this to axios. This is messy. But for requirements, we'll do it this way.
+//Expect this code to change for Sequelize.
 import { Router, Request, Response } from 'express';
-import axios from 'axios'
+import https from 'https';
 
 const router = Router();
 
 //get a random meal
 router.get('/random', async (_req: Request, res: Response) => {
-    try {
-        const response = await axios.get('https://www.themealdb.com/api/json/v1/1/random.php');
-        res.json(response.data);
-    } catch (error) {
-        console.error('ERROR: TheMealDB backend fetch error:', error);
-        res.status(500).json({ error: 'Failed to fetch data from Meal DB API. Location: api/random'});
-    }
-});
+    https.get('https://www.themealdb.com/api/json/v1/1/random.php', (apiRes) => {
+        let data = '';
 
+        apiRes.on('data', (chunk) => {
+            data += chunk;
+        });
+
+    apiRes.on('end', () => {
+        try {
+            const parsedData = JSON.parse(data);
+            res.json(parsedData.meals);
+        } catch (error) {
+            console.error('ERROR: TheMealDB backend fetch error:', error);
+            res.status(500).json({ error: 'Failed to fetch data from Meal DB API. Location: api/random'});
+        }
+    });
+//FIX ME: Not dry enough.
+    }).on('error', (error) => {
+        console.error('ERROR: TheMealDB backend fetch error:', error);
+        res.status(500).json({ error: 'Failed to fetch data from Meal DB API. Location: api/random' });
+    });
+})
 export default router;
